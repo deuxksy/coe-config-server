816dd323488d252bfbd7b16bb56ecb4f54b5b6e1
Boston <boston@Act-Boston.local>
2018-06-01 15:02:53 +0900

    first commit

---
+++ src/main/java/com/example/coeconfing/CoeConfigApplication.java
@@ -0,0 +1,16 @@
+package com.example.coeconfing;
+
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+		import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class CoeConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoeConfigApplication.class, args);
+	}
+}
