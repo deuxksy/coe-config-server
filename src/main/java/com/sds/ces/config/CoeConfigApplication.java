2552b305af28283d44084052b11bc548229acda9
Giza Act <actmember@Gizas-iMac.local>
2018-06-15 10:36:05 +0900

    add AWS Config;

--- src/main/java/com/sds/ces/config/CoeConfigApplication.java
+++ src/main/java/com/sds/ces/config/CoeConfigApplication.java
@@ -1,14 +1,33 @@
 package com.sds.ces.config;
 
+import com.netflix.appinfo.AmazonInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
 public class CoeConfigApplication {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Bean
+    public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+        EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        b.setDataCenterInfo(info);
+        b.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+        b.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+        b.setNonSecurePort(port);
+        return b;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CoeConfigApplication.class, args);
